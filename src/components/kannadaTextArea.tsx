4fe9d16260caf77449d7045b3da23b81d54dce67
Vinayaka K V <me.vinayakakv@gmail.com>
2024-11-09 12:47:24 +0530

    Add shadcn components

---
+++ src/components/kannadaTextArea.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react'
+import { useContext } from 'react'
+import { TransliterationContext } from '../lib/aksharamukha.ts'
+import { Textarea } from './ui/textarea.tsx'
+
+export const KannadaTextArea = React.forwardRef<
+  HTMLTextAreaElement,
+  Omit<React.ComponentProps<'textarea'>, 'onChange'> & {
+    onChange?: (
+      text: string,
+      kannadaText: string,
+      e: React.ChangeEvent<HTMLTextAreaElement>,
+    ) => void | Promise<void>
+  }
+>(({ onChange, ...props }, ref) => {
+  const { language, setLanguage, transliterateApi } = useContext(
+    TransliterationContext,
+  )
+  return (
+    <Textarea
+      ref={ref}
+      {...props}
+      onChange={(e) => {
+        const value = e.target.value
+        if (!value) setLanguage('')
+        let currentLanguage = language
+        if (!language) {
+          currentLanguage = transliterateApi.autoDetect(value)
+          setLanguage(currentLanguage)
+        }
+        onChange?.(
+          value,
+          transliterateApi.process({
+            from: currentLanguage,
+            to: 'Kannada',
+            input: value,
+          }),
+          e,
+        )
+      }}
+    />
+  )
+})
