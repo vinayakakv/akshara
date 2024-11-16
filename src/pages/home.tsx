347e08a50788c64210f16d96b2d76fbbabfdec39
Vinayaka K V <me.vinayakakv@gmail.com>
2024-11-16 12:09:07 +0530

    Implement home page redirecting to first tool

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,3 +1,10 @@
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 export const Home = () => {
-  return <h1 className="text-3xl">Home</h1>
+  const navigate = useNavigate()
+  useEffect(() => {
+    navigate('/tokenizer')
+  }, [navigate])
+  return null
 }
